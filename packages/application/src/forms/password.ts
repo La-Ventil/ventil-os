a5bc2de6a3ef9c7f8a73f787e400e00fbd43f29d
lutangar <johan@larriereguichet.fr>
2026-02-19 16:07:52 +0100

    feat(validation): strengthen password rules
    Require min 7 chars, lowercase, uppercase, and number.

--- packages/application/src/forms/password.ts
+++ packages/application/src/forms/password.ts
@@ -1,18 +1,16 @@
 import { z } from 'zod';
 
-export const passwordSchema = z.string().min(6, { message: 'validation.password.minLength' });
-// .max(20, { message: 'validation.password.maxLength' })
-// .refine((password) => /[A-Z]/.test(password), {
-//   message: 'validation.password.uppercaseRequired'
-// })
-// .refine((password) => /[a-z]/.test(password), {
-//   message: 'validation.password.lowercaseRequired'
-// })
-// .refine((password) => /[0-9]/.test(password), {
-//   message: 'validation.password.numberRequired'
-// })
-// .refine((password) => /[!@#$%^&*]/.test(password), {
-//   message: 'validation.password.specialCharacterRequired'
-// });
+export const passwordSchema = z
+  .string()
+  .min(7, { message: 'validation.password.minLength' })
+  .refine((password) => /[A-Z]/.test(password), {
+    message: 'validation.password.uppercaseRequired'
+  })
+  .refine((password) => /[a-z]/.test(password), {
+    message: 'validation.password.lowercaseRequired'
+  })
+  .refine((password) => /[0-9]/.test(password), {
+    message: 'validation.password.numberRequired'
+  });
 
 export const passwordConfirmationSchema = z.string().min(1, { message: 'validation.password.confirmationRequired' });
